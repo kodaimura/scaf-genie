eec6fb585c8de802eec991ae2ef7182ba7fce402
kodaimura <kodaimura123@gmail.com>
2025-04-27 11:20:15 +0900

    minor fix

--- app/helpers/auth.jl
+++ app/helpers/auth.jl
@@ -15,7 +15,7 @@ export authenticated,
 
 # Verifies the access token and returns user data if valid, or nothing if invalid.
 function authenticated()::Union{Dict{String,Any},Nothing}
-    token = get_access_token()
+    token = get_cookie("access_token")
     isnothing(token) && return nothing
     try
         return Jwt.verify_access_token(token)
@@ -29,19 +29,9 @@ function is_authenticated()::Bool
     return !isnothing(authenticated())
 end
 
-function get_access_token()::Union{String,Nothing}
-    cookies = Cookies.getcookies(Requests.request())
-    for cookie in cookies
-        if cookie.name == "access_token"
-            return cookie.value
-        end
-    end
-    return nothing
-end
-
 # Verifies the refresh token and returns user data if valid, or nothing if invalid.
 function refreshable()::Union{Dict{String,Any},Nothing}
-    token = get_refresh_token()
+    token = get_cookie("refresh_token")
     isnothing(token) && return nothing
     try
         return Jwt.verify_refresh_token(token)
@@ -55,16 +45,6 @@ function is_refreshable()::Bool
     return !isnothing(refreshable())
 end
 
-function get_refresh_token()::Union{String,Nothing}
-    cookies = Cookies.getcookies(Requests.request())
-    for cookie in cookies
-        if cookie.name == "refresh_token"
-            return cookie.value
-        end
-    end
-    return nothing
-end
-
 function access_token_cookie_header(access_token::String; options::String = "")::String
     secure = tryparse(Bool, get(ENV, "COOKIE_ACCESS_SECURE", "true")) ? "Secure" : ""
     httponly = tryparse(Bool, get(ENV, "COOKIE_ACCESS_HTTPONLY", "true")) ? "HttpOnly" : ""
@@ -75,6 +55,16 @@ function refresh_token_cookie_header(refresh_token::String; options::String = ""
     secure = tryparse(Bool, get(ENV, "COOKIE_REFRESH_SECURE", "true")) ? "Secure" : ""
     httponly = tryparse(Bool, get(ENV, "COOKIE_REFRESH_HTTPONLY", "true")) ? "HttpOnly" : ""
     return "refresh_token=$refresh_token; Path=/; $secure; $httponly; SameSite=Lax; $options"
+end
+
+function get_cookie(key::String)
+    cookies = Cookies.getcookies(Requests.request())
+    for cookie in cookies
+        if cookie.name == key
+            return cookie.value
+        end
+    end
+    return nothing
 end
 
 end
